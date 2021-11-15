7bc32373ea3a14992452a30dd7df875c6a109518
Hans Lemuet <hans@spone.net>
2021-11-15 23:01:25 +0100

    Heading node: support additional attrs

--- lib/prosemirror_to_html/nodes/heading.rb
+++ lib/prosemirror_to_html/nodes/heading.rb
@@ -4,7 +4,19 @@ module ProsemirrorToHtml
       @node_type = 'heading'
 
       def tag
-        "h#{@node.attrs.level}"
+        [
+          {
+            tag: "h#{@node.attrs.level}",
+            attrs: tag_attrs
+          }
+        ]
+      end
+
+      private
+
+      def tag_attrs
+        attrs_hash = @node.attrs.to_h
+        attrs_hash.slice(*attrs_hash.keys - %i[level])
       end
     end
   end
--- spec/nodes/heading_spec.rb
+++ spec/nodes/heading_spec.rb
@@ -23,4 +23,30 @@ RSpec.describe ProsemirrorToHtml::Nodes::Heading do
     renderer = ProsemirrorToHtml::Renderer.new
     expect(renderer.render(json)).to eq html
   end
+
+  it 'renders correctly with an ID' do
+    json = {
+      type: 'doc',
+      content: [
+        {
+          type: 'heading',
+          attrs: {
+            level: 2,
+            id: 'example-headline'
+          },
+          content: [
+            {
+              type: 'text',
+              text: 'Example Headline'
+            }
+          ]
+        }
+      ]
+    }
+
+    html = '<h2 id="example-headline">Example Headline</h2>'
+
+    renderer = ProsemirrorToHtml::Renderer.new
+    expect(renderer.render(json)).to eq html
+  end
 end

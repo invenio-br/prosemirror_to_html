14eeb304838920b49d69b4ed364406af5ada0296
Hans Lemuet <hans@spone.net>
2021-11-16 00:00:48 +0100

    Support camelcased Node types

--- lib/prosemirror_to_html/nodes/node.rb
+++ lib/prosemirror_to_html/nodes/node.rb
@@ -13,7 +13,7 @@ module ProsemirrorToHtml
       end
 
       def matching
-        return @node.type == self.class.node_type if @node.type
+        return underscore(@node.type) == self.class.node_type if @node.type
 
         false
       end
@@ -28,6 +28,20 @@ module ProsemirrorToHtml
 
       def text
         nil
+      end
+
+      private
+
+      # from https://github.com/rails/rails/blob/83217025a171593547d1268651b446d3533e2019/activesupport/lib/active_support/inflector/methods.rb#L92
+      def underscore(camel_cased_word)
+        return camel_cased_word unless /[A-Z-]|::/.match?(camel_cased_word)
+
+        word = camel_cased_word.to_s.gsub("::", "/")
+        word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
+        word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+        word.tr!("-", "_")
+        word.downcase!
+        word
       end
     end
   end
--- spec/nodes/horizontal_rule_spec.rb
+++ spec/nodes/horizontal_rule_spec.rb
@@ -32,4 +32,38 @@ RSpec.describe ProsemirrorToHtml::Nodes::HorizontalRule do
     renderer = ProsemirrorToHtml::Renderer.new
     expect(renderer.render(json)).to eq html
   end
+
+  it 'renders correctly with camelCase' do
+    json = {
+      type: 'doc',
+      content: [
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'text',
+              text: 'some text'
+            },
+          ]
+        },
+        {
+          type: 'horizontalRule'
+        },
+        {
+          type: 'paragraph',
+          content: [
+            {
+              type: 'text',
+              text: 'some more text'
+            }
+          ]
+        }
+      ]
+    }
+
+    html = '<p>some text</p><hr><p>some more text</p>'
+
+    renderer = ProsemirrorToHtml::Renderer.new
+    expect(renderer.render(json)).to eq html
+  end
 end

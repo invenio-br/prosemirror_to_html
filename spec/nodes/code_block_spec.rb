aeb9e5bd6247d0c804952986c1405ec0e12012ef
Daniel Moreira <danieldenis01@gmail.com>
2025-04-12 15:57:24 -0300

    Adiciona Code Language (#1)

--- spec/nodes/code_block_spec.rb
+++ spec/nodes/code_block_spec.rb
@@ -20,4 +20,18 @@ RSpec.describe ProsemirrorToHtml::Nodes::CodeBlock do
     renderer = ProsemirrorToHtml::Renderer.new
     expect(renderer.render(json)).to eq html
   end
+
+  it 'renders a code block with language class' do
+    node = {
+      'type' => 'code_block',
+      'attrs' => { 'language' => 'ruby' },
+      'content' => [{ 'type' => 'text', 'text' => 'puts "Hello, world!"' }]
+    }
+
+    code_block = ProsemirrorToHtml::Nodes::CodeBlock.new(node)
+    expect(code_block.tag).to eq([
+      { tag: 'pre' },
+      { tag: 'code', attrs: { class: 'language-ruby' } }
+    ])
+  end
 end

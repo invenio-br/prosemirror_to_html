30491727363d4ac53c7709dfaa9e235e2c84bbad
Daniel Moreira <danieldenis01@gmail.com>
2025-04-12 15:56:27 -0300

    Adiciona Code Language

--- lib/prosemirror_to_html/nodes/code_block.rb
+++ lib/prosemirror_to_html/nodes/code_block.rb
@@ -3,6 +3,22 @@ module ProsemirrorToHtml
     class CodeBlock < Node
       @node_type = 'code_block'
       @tag_name = ['pre', 'code']
+
+      def matching
+        @node['type'] == 'code_block'
+      end
+
+      def tag
+        language = @node.dig('attrs', 'language')
+        if language
+          [
+            { tag: 'pre' },
+            { tag: 'code', attrs: { class: "language-#{language}" } }
+          ]
+        else
+          ['pre', 'code']
+        end
+      end
     end
   end
 end

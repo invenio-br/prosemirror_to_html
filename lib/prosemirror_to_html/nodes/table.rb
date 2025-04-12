a331bc2411047df0cf6bb9683b12ffdee49a5b56
Daniel Moreira <danieldenis01@gmail.com>
2025-04-12 17:10:41 -0300

    Ajust Tags for Bootstrap

--- lib/prosemirror_to_html/nodes/table.rb
+++ lib/prosemirror_to_html/nodes/table.rb
@@ -3,6 +3,14 @@ module ProsemirrorToHtml
     class Table < Node
       @node_type = 'table'
       @tag_name = ['table', 'tbody']
+
+      def tag
+        tags = []
+        tags << { tag: "div", attrs: { class: "table-responsive" } }
+        tags << { tag: "table", attrs: { class: "table table-striped table-bordered" } }
+        tags << { tag: "tbody" }
+        tags
+      end
     end
   end
 end
